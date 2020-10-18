ce77c57a00add0dfad11629f55dd3957f4423c0f
Guezin <leandroguezinjunior@gmail.com>
2020-10-18 16:29:02 -0300

    adicionando roteamento

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import Routes from './routes'
+
 import GlobalStyle from './styles/global'
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <h1>Olá Mundo!</h1>
+      <Routes />
     </>
   )
 }
